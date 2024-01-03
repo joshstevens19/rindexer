06eae1c175b379dc5d6f0fc9371a2515c28ae7ae
Josh Stevens <joshstevens19@hotmail.co.uk>
2024-01-03 18:21:22 +0000

    feat: helpers

---
+++ rindexer_core/src/helpers/mod.rs
@@ -0,0 +1,23 @@
+pub fn capitalize_first_letter(s: &str) -> String {
+    s.chars()
+        .enumerate()
+        .map(|(i, c)| {
+            if i == 0 {
+                c.to_uppercase().to_string()
+            } else {
+                c.to_string()
+            }
+        })
+        .collect()
+}
+
+pub fn camel_to_snake(name: &str) -> String {
+    let mut snake_case = String::new();
+    for (i, ch) in name.chars().enumerate() {
+        if ch.is_uppercase() && i != 0 {
+            snake_case.push('_');
+        }
+        snake_case.push(ch.to_lowercase().next().unwrap());
+    }
+    snake_case
+}
