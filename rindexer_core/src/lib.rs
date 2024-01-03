9e4efb14a7bcd4e9f1fb98a590efb40fd9ede480
Josh Stevens <joshstevens19@hotmail.co.uk>
2024-01-03 18:21:45 +0000

    feat: core base files

---
+++ rindexer_core/src/lib.rs
@@ -0,0 +1,19 @@
+pub mod generator;
+mod helpers;
+mod manifest;
+mod node;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
