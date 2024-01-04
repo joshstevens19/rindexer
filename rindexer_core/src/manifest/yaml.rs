46d5e7efd46c5c72151b18a43e417907d5604a81
Josh Stevens <joshstevens19@hotmail.co.uk>
2024-01-04 18:00:10 +0000

    feat: enhanace manifest

--- rindexer_core/src/manifest/yaml.rs
+++ rindexer_core/src/manifest/yaml.rs
@@ -6,68 +6,69 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Manifest {
-    description: String,
-    repository: String,
-    indexers: Vec<Indexer>,
-    mapping: Mapping,
-    networks: Vec<Network>,
+    pub description: Option<String>,
+    pub repository: Option<String>,
+    pub indexers: Vec<Indexer>,
+    pub networks: Vec<Network>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Indexer {
-    name: String,
-    networks: Vec<String>,
-    source: Vec<Source>,
-    context: Context,
+    pub name: String,
+    pub networks: Vec<String>,
+    pub sources: Vec<Source>,
+    pub context: Option<Context>,
+    pub mappings: Mappings,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Source {
-    address: Option<String>,
-    network: Option<String>,
+    pub name: String,
+    pub address: String,
+    pub network: String,
     #[serde(rename = "startBlock")]
-    start_block: Option<u64>,
+    pub start_block: Option<u64>,
     #[serde(rename = "endBlock")]
-    end_block: Option<u64>,
+    pub end_block: Option<u64>,
     #[serde(rename = "pollingEvery")]
-    polling_every: Option<u64>,
-    abi: Option<String>,
+    pub polling_every: Option<u64>,
+    pub abi: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Context {
-    contracts: Vec<Contract>,
+    pub contracts: Vec<Contract>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Contract {
-    abi: String,
-    name: String,
-    network: String,
-    address: String,
+    pub abi: String,
+    pub name: String,
+    pub network: String,
+    pub address: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Mapping {
-    abis: Vec<Abi>,
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Mappings {
+    pub abis: Vec<ABI>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Abi {
-    name: String,
-    file: String,
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct ABI {
+    pub name: String,
+    pub file: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Network {
-    name: String,
+    pub name: String,
     #[serde(rename = "chainId")]
-    chain_id: u32,
-    url: String,
+    pub chain_id: u32,
+    pub url: String,
     #[serde(rename = "maxBlockRange")]
-    max_block_range: Option<u64>,
+    pub max_block_range: Option<u64>,
     #[serde(rename = "maxConcurrency")]
-    max_concurrency: Option<u32>,
+    pub max_concurrency: Option<u32>,
 }
 
 pub fn read_manifest(file_path: &str) -> Result<Manifest, Box<dyn Error>> {
@@ -77,4 +78,17 @@ pub fn read_manifest(file_path: &str) -> Result<Manifest, Box<dyn Error>> {
 
     let manifest: Manifest = serde_yaml::from_str(&contents)?;
     Ok(manifest)
+}
+
+#[cfg(test)]
+mod tests {
+    const MANIFEST: &str =
+        "/Users/joshstevens/code/rindexer/rindexer_core/external-examples/manifest-example.yaml";
+
+    #[test]
+    fn read_works() {
+        let manifest = super::read_manifest(MANIFEST).unwrap();
+
+        println!("{:?}", manifest);
+    }
 }
