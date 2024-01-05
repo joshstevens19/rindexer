92c9ec1572aa0a0030973329c064e2bfc815a421
Josh Stevens <joshstevens19@hotmail.co.uk>
2024-01-05 23:06:46 +0000

    cleanup

--- rindexer_core/src/indexer/start.rs
+++ rindexer_core/src/indexer/start.rs
@@ -7,19 +7,12 @@ use ethers::{
 use crate::generator::event_callback_registry::EventCallbackRegistry;
 
 pub async fn start(registry: EventCallbackRegistry) -> () {
-    // registry.trigger_event(
-    //     "0xc906270cebe7667882104effe64262a73c422ab9176a111e05ea837b021065fc",
-    //     &(1),
-    // );
-
     // 1. Get the current block numbers for networks
     // 2. Get the logs between blocks for each event and resync back
     // 3. When hits the head block, start listening to new blocks and pushing activity to check for new logs
     // 4. Use blooms to filter out logs that are not relevant
 
     for event in &registry.events {
-        println!("hey {:?}", (&event.source, &event.topic_id));
-
         let filter: Filter = Filter::new()
             .address(event.source.address.parse::<Address>().unwrap())
             .topic0(event.topic_id.parse::<H256>().unwrap())
