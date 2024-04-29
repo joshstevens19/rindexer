61ef7c138e9059225efb9a2cd3289b902fef8c6a
Josh Stevens <joshstevens19@hotmail.co.uk>
2024-04-29 13:46:04 +0100

    feat: build out logs with stream

--- rindexer_core/src/indexer/fetch_logs.rs
+++ rindexer_core/src/indexer/fetch_logs.rs
@@ -7,6 +7,8 @@ use std::future::Future;
 use std::pin::Pin;
 use std::str::FromStr;
 use std::sync::Arc;
+use tokio::sync::mpsc;
+use tokio_stream::wrappers::UnboundedReceiverStream;
 
 struct RetryWithBlockRangeResult {
     from: BlockNumber,
@@ -18,7 +20,8 @@ fn retry_with_block_range(error: &JsonRpcError) -> Option<RetryWithBlockRangeRes
     let error_message = &error.message;
 
     // alchemy - https://github.com/ponder-sh/ponder/blob/main/packages/utils/src/getLogsRetryHelper.ts
-    let re = Regex::new(r"this block range should work: \[(0x[0-9a-fA-F]+),\s*(0x[0-9a-fA-F]+)]").unwrap();
+    let re = Regex::new(r"this block range should work: \[(0x[0-9a-fA-F]+),\s*(0x[0-9a-fA-F]+)]")
+        .unwrap();
     if let Some(captures) = re.captures(error_message) {
         let start_block = captures.get(1).unwrap().as_str();
         println!("start_block: {:?}", start_block);
@@ -39,6 +42,7 @@ fn retry_with_block_range(error: &JsonRpcError) -> Option<RetryWithBlockRangeRes
     None
 }
 
+// TODO! can be removed if no use for it as we stream info across
 pub fn fetch_logs<M: Middleware + Clone + Send + 'static>(
     provider: Arc<M>,
     filter: Filter,
@@ -77,4 +81,60 @@ pub fn fetch_logs<M: Middleware + Clone + Send + 'static>(
     }
 
     Box::pin(inner_fetch_logs(provider, filter))
+}
+
+pub fn fetch_logs_stream<M: Middleware + Clone + Send + 'static>(
+    provider: Arc<M>,
+    initial_filter: Filter,
+) -> impl tokio_stream::Stream<Item = Result<Vec<Log>, Box<<M as Middleware>::Error>>> + Send + Unpin
+{
+    let (tx, rx) = mpsc::unbounded_channel();
+
+    tokio::spawn(async move {
+        let snapshot_to_block = initial_filter.clone().get_to_block().unwrap();
+        let mut current_filter = initial_filter;
+        loop {
+            match provider.get_logs(&current_filter).await {
+                Ok(logs) => {
+                    if logs.is_empty() {
+                        println!("No logs left");
+                        break;
+                    }
+
+                    if tx.send(Ok(logs.clone())).is_err() {
+                        println!("Failed to send logs to stream");
+                        break;
+                    }
+
+                    if let Some(last_log) = logs.last() {
+                        // TODO! we should not skip a block as we might miss logs in the same block
+                        let next_block = last_log.block_number.unwrap() + U64::from(1);
+                        current_filter = current_filter
+                            .from_block(next_block)
+                            .to_block(snapshot_to_block);
+                        println!("Updated filter: {:?}", current_filter);
+                    }
+                }
+                Err(err) => {
+                    println!("Failed to fetch logs: {:?}", err);
+                    let json_rpc_error = err.as_error_response();
+                    if let Some(json_rpc_error) = json_rpc_error {
+                        let retry_result = retry_with_block_range(json_rpc_error);
+                        if let Some(retry_result) = retry_result {
+                            current_filter = current_filter
+                                .from_block(retry_result.from)
+                                .to_block(retry_result.to);
+                            println!("Retrying with block range: {:?}", current_filter);
+                            continue;
+                        }
+                    }
+
+                    let _ = tx.send(Err(Box::new(err)));
+                    break;
+                }
+            }
+        }
+    });
+
+    UnboundedReceiverStream::new(rx)
 }
