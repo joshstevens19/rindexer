5765e854abd1d62647929d81e2acab89e430d3ed
Josh Stevens <joshstevens19@hotmail.co.uk>
2024-05-16 00:07:37 +0100

    fix: resolve live indexing issue

--- rindexer_demo/src/indexers/lens_registry_example/erc20_filter.rs
+++ rindexer_demo/src/indexers/lens_registry_example/erc20_filter.rs
@@ -11,7 +11,7 @@ async fn transfer_handler(registry: &mut EventCallbackRegistry) {
                     TransferEvent::new(
                         Arc::new(|results, context| {
                             Box::pin(async move {
-                                println!("Transfer event: {:?}", results);
+                                // println!("Transfer event: {:?}", results);
                                 for result in results {
                                     context.csv.append(vec![format!("{:?}", result.tx_information.address),format!("{:?}", result.event_data.from,),format!("{:?}", result.event_data.to,),result.event_data.value.to_string(),format!("{:?}", result.tx_information.transaction_hash.unwrap()),result.tx_information.block_number.unwrap().to_string(),result.tx_information.block_hash.unwrap().to_string()]).await.unwrap();
                                     context.database.execute("INSERT INTO lens_registry_example.transfer (contract_address, \"from\", \"to\", \"value\", \"tx_hash\", \"block_number\", \"block_hash\") VALUES($1, $2, $3, $4, $5, $6, $7)",&[&EthereumSqlTypeWrapper::Address(&result.tx_information.address),&EthereumSqlTypeWrapper::Address(&result.event_data.from),&EthereumSqlTypeWrapper::Address(&result.event_data.to),&EthereumSqlTypeWrapper::U256(&result.event_data.value),&EthereumSqlTypeWrapper::H256(&result.tx_information.transaction_hash.unwrap()),&EthereumSqlTypeWrapper::U64(&result.tx_information.block_number.unwrap()),&EthereumSqlTypeWrapper::H256(&result.tx_information.block_hash.unwrap())]).await.unwrap();
