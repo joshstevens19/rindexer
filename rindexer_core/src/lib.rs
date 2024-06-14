0ec1fed0c02f44b1e286dd3fdc0afa053975a01a
Josh Stevens <joshstevens19@hotmail.co.uk>
2024-06-14 12:41:28 +0100

    feat: delete CLI command

--- rindexer_core/src/lib.rs
+++ rindexer_core/src/lib.rs
@@ -4,7 +4,7 @@ pub mod indexer;
 pub mod manifest;
 
 mod database;
-pub use database::postgres::{setup_postgres, EthereumSqlTypeWrapper, PostgresClient};
+pub use database::postgres::{setup_postgres, EthereumSqlTypeWrapper, PostgresClient, drop_tables_for_indexer_sql};
 
 mod simple_file_formatters;
 pub use simple_file_formatters::csv::AsyncCsvAppender;
