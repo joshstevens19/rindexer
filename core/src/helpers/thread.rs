07c7d9d4546910467856d1b7b957eaa5549b4663
Josh Stevens <joshstevens19@hotmail.co.uk>
2024-07-06 23:55:31 +0100

    tests: add unit tests

--- core/src/helpers/thread.rs
+++ core/src/helpers/thread.rs
@@ -27,3 +27,51 @@ pub fn set_thread_no_logging() {
         *thread_stderr.borrow_mut() = Some(Box::new(NullWriter));
     });
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Write;
+
+    #[test]
+    fn test_null_writer_write() {
+        let mut writer = NullWriter;
+        let data = b"hello";
+        let result = writer.write(data);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), data.len());
+    }
+
+    #[test]
+    fn test_null_writer_flush() {
+        let mut writer = NullWriter;
+        let result = writer.flush();
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn test_thread_no_logging_stdout() {
+        set_thread_no_logging();
+        THREAD_STDOUT.with(|thread_stdout| {
+            let mut thread_stdout = thread_stdout.borrow_mut();
+            let writer = thread_stdout.as_mut().unwrap();
+            let data = b"hello";
+            let result = writer.write(data);
+            assert!(result.is_ok());
+            assert_eq!(result.unwrap(), data.len());
+        });
+    }
+
+    #[test]
+    fn test_thread_no_logging_stderr() {
+        set_thread_no_logging();
+        THREAD_STDERR.with(|thread_stderr| {
+            let mut thread_stderr = thread_stderr.borrow_mut();
+            let writer = thread_stderr.as_mut().unwrap();
+            let data = b"error";
+            let result = writer.write(data);
+            assert!(result.is_ok());
+            assert_eq!(result.unwrap(), data.len());
+        });
+    }
+}
