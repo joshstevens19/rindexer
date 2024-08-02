321c175b49c3a92ea3078763e134016de35a4669
Josh Stevens <joshstevens19@hotmail.co.uk>
2024-08-02 20:35:34 +0100

    refactor: move stream manifest to own file

---
+++ core/src/manifest/stream.rs
@@ -0,0 +1,14 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct SNSStreamConfig {
+    pub prefix_id: Option<String>,
+    pub topic_arn: String,
+    pub networks: Vec<String>,
+    pub events: Vec<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct StreamsConfig {
+    pub sns: Option<Vec<SNSStreamConfig>>,
+}
