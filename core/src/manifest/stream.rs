06dfa0fdc5ee0b4fba04840c1790bbe42c78cc8d
Josh Stevens <joshstevens19@hotmail.co.uk>
2024-08-04 12:06:33 +0100

    feat: kafka

--- core/src/manifest/stream.rs
+++ core/src/manifest/stream.rs
@@ -30,8 +30,29 @@ pub struct RabbitMQStreamConfig {
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct KafkaStreamQueueConfig {
+    pub topic: String,
+    pub key: String,
+    pub networks: Vec<String>,
+    pub events: Vec<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct KafkaStreamConfig {
+    pub brokers: Vec<String>,
+    pub security_protocol: String,
+    pub sasl_mechanisms: Option<String>,
+    pub sasl_username: Option<String>,
+    pub sasl_password: Option<String>,
+    pub acks: String,
+    pub dr_msg_cb: bool,
+    pub topics: Vec<KafkaStreamQueueConfig>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct StreamsConfig {
     pub sns: Option<Vec<SNSStreamConfig>>,
     pub webhook: Option<Vec<WebhookStreamConfig>>,
     pub rabbitmq: Option<RabbitMQStreamConfig>,
+    pub kafka: Option<KafkaStreamConfig>,
 }
