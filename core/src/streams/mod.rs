716bde5ed2d3b3d3d5206ff82c6ea9bc566c9e46
Josh Stevens <joshstevens19@hotmail.co.uk>
2024-08-04 09:45:28 +0100

    feat: rabbitmq

--- core/src/streams/mod.rs
+++ core/src/streams/mod.rs
@@ -2,7 +2,10 @@ mod sns;
 pub use sns::SNS;
 
 mod webhook;
-pub use webhook::Webhook;
+pub use webhook::{Webhook, WebhookError};
+
+mod rabbitmq;
+pub use rabbitmq::{RabbitMQ, RabbitMQError};
 
 mod clients;
 pub use clients::{EventMessage, StreamsClients};
