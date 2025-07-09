0f5d6455e8736121bdf1e9d39348c77027fcfd2b
Jaggad <jack@turboclicks.com.au>
2025-07-09 16:26:17 +0700

    Docs

--- documentation/vocs.config.tsx
+++ documentation/vocs.config.tsx
@@ -82,6 +82,10 @@ export default defineConfig({
               link: "/docs/start-building/yaml-config/top-level-fields",
             },
             {
+              text: "Config",
+              link: "/docs/start-building/yaml-config/config",
+            },
+            {
               text: "Networks",
               link: "/docs/start-building/yaml-config/networks",
             },
