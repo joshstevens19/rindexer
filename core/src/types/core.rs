3c61d5808a60558e3e90ef3662b52a28f4669264
Josh Stevens <joshstevens19@hotmail.co.uk>
2025-09-26 13:13:51 +0100

    feat: add cloudflare queues to the streams (#314)

--- core/src/types/core.rs
+++ core/src/types/core.rs
@@ -1,32 +1,5 @@
+use alloy::dyn_abi::DynSolValue;
 use alloy::json_abi::Param;
-use alloy::{
-    dyn_abi::DynSolValue,
-    primitives::{Address, BlockHash, TxHash, U256, U64},
-};
-use serde::{Deserialize, Serialize};
-
-/// Metadata inside a log
-#[allow(dead_code)]
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
-pub struct LogMeta {
-    /// Address from which this log originated
-    pub address: Address,
-
-    /// The block in which the log was emitted
-    pub block_number: U64,
-
-    /// The block hash in which the log was emitted
-    pub block_hash: BlockHash,
-
-    /// The transaction hash in which the log was emitted
-    pub transaction_hash: TxHash,
-
-    /// Transactions index position log was created from
-    pub transaction_index: U64,
-
-    /// Log index position in the block
-    pub log_index: U256,
-}
 
 /// Decoded log param.
 #[derive(Debug, PartialEq, Clone)]
