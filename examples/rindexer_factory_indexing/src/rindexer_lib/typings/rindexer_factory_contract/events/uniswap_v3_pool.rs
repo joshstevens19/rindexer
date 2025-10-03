1d051bc0fba859470b8faaf9d7c0aaf5af11331c
Paweł Lula <desfero@users.noreply.github.com>
2025-10-03 17:34:35 +0200

    Fix lints

--- examples/rindexer_factory_indexing/src/rindexer_lib/typings/rindexer_factory_contract/events/uniswap_v3_pool.rs
+++ examples/rindexer_factory_indexing/src/rindexer_lib/typings/rindexer_factory_contract/events/uniswap_v3_pool.rs
@@ -247,7 +247,7 @@ where
             r"/Users/pawellula/RustroverProjects/rindexer/cli/../examples/rindexer_factory_indexing/generated_csv/UniswapV3Pool/uniswapv3pool-swap.csv",
         );
         if !Path::new(r"/Users/pawellula/RustroverProjects/rindexer/cli/../examples/rindexer_factory_indexing/generated_csv/UniswapV3Pool/uniswapv3pool-swap.csv").exists() {
-            csv.append_header(vec!["contract_address".into(), "sender".into(), "recipient".into(), "amount_0".into(), "amount_1".into(), "sqrt_price_x96".into(), "liquidity".into(), "tick".into(), "tx_hash".into(), "block_number".into(), "block_hash".into(), "network".into(), "tx_index".into(), "log_index".into()].into())
+            csv.append_header(vec!["contract_address".into(), "sender".into(), "recipient".into(), "amount_0".into(), "amount_1".into(), "sqrt_price_x96".into(), "liquidity".into(), "tick".into(), "tx_hash".into(), "block_number".into(), "block_hash".into(), "network".into(), "tx_index".into(), "log_index".into()])
                 .await
                 .expect("Failed to write CSV header");
         }
@@ -397,7 +397,7 @@ where
         let index_event_in_order = contract_details
             .index_event_in_order
             .as_ref()
-            .map_or(false, |vec| vec.contains(&event_name.to_string()));
+            .is_some_and(|vec| vec.contains(&event_name.to_string()));
 
         // Expect providers to have been initialized, but it's an async init so this should
         // be fast but for correctness we must await each future.
@@ -432,7 +432,7 @@ where
                             .networks
                             .iter()
                             .find(|n| n.name == c.network)
-                            .map_or(false, |n| n.disable_logs_bloom_checks.unwrap_or_default()),
+                            .is_some_and(|n| n.disable_logs_bloom_checks.unwrap_or_default()),
                     }
                 })
                 .collect(),
